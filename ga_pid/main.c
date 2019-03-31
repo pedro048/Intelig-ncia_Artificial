e371f1b745c96f9c5374a810846d6eab59842009
Pedro Victor Andrade Alves <37122281+pedro048@users.noreply.github.com>
2019-03-31 17:23:42 -0300

    Update main.c

--- ga_pid/main.c
+++ ga_pid/main.c
@@ -9,8 +9,10 @@
 #define ADPORTA 0
 #define VEL_MAX_MOTOR 100.0
 #define TAM 4 //quantidade de cromossomos
-#define SETPOINT = 40.0
-#define VEL = 20.0 //simula a velocidade do motor em um dado momento
+#define SETPOINT  40.0
+#define VEL  20.0 //simula a velocidade do motor em um dado momento
+
+float erro = 0, erro_ant= 0, erro_dif = 0, erro_int = 0, dt= 0;
 
 
 typedef struct individuo {
@@ -21,28 +23,26 @@ typedef struct individuo {
 }INDIVIDUO;
 
 //void gerarPopulacao (INDIVIDUO populacao[TAM]){
-void gerarPopulacao(){
-	/*
-    int i=0, j=0;
-    float min = 0.10, max = 30;
-	*/
-	int i;
-	int j;
-	INDIVIDUO populacao[TAM];
+void gerarPopulacao(INDIVIDUO populacao[TAM]){
+	int i, j;
+    //RAND_MAX eh definido como 0x7fffffff
     for (i=0; i<TAM; i++){
         for (j=0; j<3; j++){
-            populacao[i].cromossomo[j] = (float)(rand()%40);
+            populacao[i].cromossomo[j] = 100* ((float)rand()/RAND_MAX);
         }
     }
 }
 
-
 //float altura, erro, erro_dif, erro_int, setpoint, kp, ki, kd, dt;
 
 //float pid(float vel, float erro, float erro_dif, float erro_int, float setpoint, float kp, float ki, float kd, float dt){
-float pid(float vel, float setpoint, float kp, float ki, float kd, float dt){
-  float erro_ant = erro;
+float pid(float vel, float setpoint, float kp, float ki, float kd, clock_t tempo){
+  
+  erro_ant = erro;
   erro = setpoint - vel;
+  tempo = clock() - tempo;
+  dt = (float)tempo/CLOCKS_PER_SEC;
+  //erro_dif = (erro - erro_ant)/dt;
   erro_dif = (erro - erro_ant)/dt;
   erro_int += erro_ant*dt;
   float saida = erro*kp + erro_dif*kd + erro_int*ki;
@@ -55,66 +55,48 @@ float pid(float vel, float setpoint, float kp, float ki, float kd, float dt){
 }
 
 //setar os parametros do pid
-void chamarPID (INDIVIDUO crom){
-    //float altura, erro, erro_dif, erro_int, setpoint, kp, ki, kd, dt;
+void chamarPID (INDIVIDUO crom, clock_t tempo){
+    float kp, ki, kd, dt;
     kp = crom.cromossomo[0];
     ki = crom.cromossomo[1];
     kd = crom.cromossomo[2];
-	
-	crom.spid = pid(VEL, SETPOINT, kp, ki, kd); //Associa um valor de solucao do pid a cada cromossomo	
+    //dt = rand () % 11;
+
+	crom.spid = pid(VEL, SETPOINT, kp, ki, kd, tempo); //Associa um valor de solucao do pid a cada cromossomo
 }
 
-//troca um dos tres bits do pai com algum dos tres da mae
-//void cruzamento (INDIVIDUO populacao[TAM], int i_pai, int i_mae){
-	
-void cruzamento (INDIVIDUO populacao[TAM]){ // A polpulacao passada tem a apenas os cromossomos aptos para o cruzamento
-    int indice1 = rand() % 3;
-	int indice2 = rand() % 3;
-	int indice3 = rand() % 3;
-	int indice4 = rand() % 3;
-	
-    float aux1,aux2,aux3,aux4;
+void cruzamento (INDIVIDUO populacao[TAM], int i_pai, int i_mae){
+    int indice1 = rand() % 3, indice2;
+    float aux1,aux2;
 
-    aux1 = populacao[0].cromossomo[indice1];
-	
+    aux1 = populacao[i_pai].cromossomo[indice1];
     do{
         indice2 = rand() % 3;
     }while(indice1 == indice2);
-    aux2 = populacao[1].cromossomo[indice2];
+    aux2 = populacao[i_mae].cromossomo[indice2];
 
-    populacao[0].cromossomo[indice1] = aux2;
-    populacao[1].cromossomo[indice2] = aux1;
-	
-	aux3 = populacao[2].cromossomo[indice3];
-	
-    do{
-        indice4 = rand() % 3;
-    }while(indice3 == indice4);
-    aux4 = populacao[3].cromossomo[indice4];
-
-    populacao[2].cromossomo[indice3] = aux4;
-    populacao[3].cromossomo[indice4] = aux3;
-	
-	// A populacao anterior he substituida pelos filhos
+    populacao[i_pai].cromossomo[indice1] = aux2;
+    populacao[i_mae].cromossomo[indice2] = aux1;
 
 }
 
+void mutacao(INDIVIDUO popul[TAM]){
+    float mut = rand()/RAND_MAX;
+    float taxa_mutacao = 0.2;
+    int indiv = rand() % TAM;
+    int gen = rand() % 3;
 
-void mutacao(int populacao[][3], int tam){
-    int mut = rand() % 101;
-    int taxa_mutacao = 20;
-    //int i;
     if(mut<=taxa_mutacao){
-
+        popul[indiv].cromossomo[gen] = 100* ((float)rand()/RAND_MAX);
     }
 }
 
 //calcula avaliacao
-void fitneas(INDIVIDUO popul[TAM]){ 
-	
+void fitneas(INDIVIDUO popul[TAM]){
+
 	int i;
     for(i=0; i<TAM; i++){
-        popul[i].erroInd = SETPOINT - VEL; 
+        popul[i].erroInd = SETPOINT - VEL;
     }
 
 }
@@ -143,64 +125,77 @@ void selecao (INDIVIDUO populacao[TAM]){
     }
 }
 
+void imprimir(INDIVIDUO populacao[TAM]){
+    int i,j;
+    for (i=0; i<TAM; i++){
+        for(j=0; j<3; j++){
+            printf("%.2f   ", populacao[i].cromossomo[j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 
+}
 
 int main (){
-    //srand(time(NULL));
+	
+    srand(time(NULL));
+    clock_t tempo;
+    tempo = clock();
     INDIVIDUO populacao[TAM];
 
-    populacao[0].cromossomo[0] = 2.56; //kp
-    populacao[0].cromossomo[1] = 3.67; //ki
-    populacao[0].cromossomo[2] = 4.24; //kd
-   // populacao[0].erro = fitneas();
-
-
-    populacao[1].cromossomo[0] = 5.23; //kp
-    populacao[1].cromossomo[1] = 6.78; //ki
-    populacao[1].cromossomo[2] = 1.56; //kd
-    //populacao[1].erro = fitneas();
-
-
-    populacao[2].cromossomo[0] = 4.78; //kp
-    populacao[2].cromossomo[1] = 6.23; //ki
-    populacao[2].cromossomo[2] = 1.34; //kd
-    //populacao[2].erro = fitneas();
-
-
-    populacao[3].cromossomo[0] = 1.43; //kp
-    populacao[3].cromossomo[1] = 5.43; //ki
-    populacao[3].cromossomo[2] = 4.43; //kd
-
-    //gerarPopulacao(populacao);
-    //populacao[3].erro = fitneas();
+    printf("Geracao Inicial:\n");
+    gerarPopulacao(populacao);
+	int i;
+	for(i=0; i<TAM; i++){
+		chamarPID(populacao[i], tempo);
+	}
     fitneas(populacao);
-
-    int qtd_geracoes = 10, indice = 0, cont = 0;
-    imprimir(populacao);
-    do{
-        indice++;
-        cont++;
-        selecao(populacao);
-        for(; indice<TAM; indice+=2){
-
-            cruzamento(populacao,indice,indice+1);
-        }
-        fitneas(populacao);
-        imprimir(populacao);
-
-    } while(cont <= qtd_geracoes);
-    /*
-    //TESTE DO CRUZAMENTO
     imprimir(populacao);
     printf("\n");
-    cruzamento (populacao,0,1);
-    imprimir(populacao);
+
+    int qtd_geracoes=5, i=0, cont=0;
+    while(cont < qtd_geracoes){
+        cont++;
+        selecao(populacao);
+
+        //ALGO ESTA INTERFERINDO NESTE FOR E FAZENDO AS GERACOES FICAREM ERRADAS
+        for(; i<TAM; i+=2){
+            cruzamento(populacao,i,i+1);
+        }
+
+        mutacao(populacao);
+	int j;
+	for(j=0; j<TAM; j++){
+		chamarPID(populacao[j], tempo);
+	}
+        fitneas(populacao);
+        printf("Geracao %i:\n",cont);
+        imprimir(populacao);
+
+    }
+    printf("\n\n\n\n");
+    /*
+    //CAPTURA O VALOR DOS SEGUNDOS DO RELOGIO DO COMPUTADOR
+    struct tm *local;
+    time_t t;
+    t = time(NULL);
+    local = localtime(&t);
+    printf("Tempo em segundos: %i", *local);
     */
 
 
+  //TESTE DO CRUZAMENTO
+    /*
+     for(; indice<TAM; indice+=2){
 
+            cruzamento(populacao,indice,indice+1);
+            imprimir(populacao);
+             printf("\n");
+        }
 
-
+    //populacao[3].erro = fitneas();
+    */
      /*
       kp=1.0;
       ki=0.2;
