23e14450dc9b0de86a021d7f0e14b75be0ad5029
Pedro Victor Andrade Alves <37122281+pedro048@users.noreply.github.com>
2019-06-16 17:37:50 -0300

    Update Projeto3_IA_fuzzy.py

--- Projeto3_IA_fuzzy.py
+++ Projeto3_IA_fuzzy.py
@@ -12,16 +12,22 @@ import matplotlib.pyplot as plt
 
 # antecedentes relacionados com a comida
 qualidade_atendimento = ctrl.Antecedent(np.arange(0, 11, 1), 'qualidade_atendimento')
-# ---------------------------------------------------------------------------------------
-tempo_entrega_pedido = ctrl.Antecedent(np.arange(0, 11, 1), 'tempo_entrega_pedido')
-# ---------------------------------------------------------------------------------------
-pedido_certo = ctrl.Antecedent(np.arange(0, 11, 1), 'pedido_certo')
-# ---------------------------------------------------------------------------------------
 
-qualidade_atendimento.automf(3)
-tempo_entrega_pedido.automf(3)
-pedido_certo.automf(3)
-'''
+tempo_entrega_pedido = ctrl.Antecedent(np.arange(0, 11, 1), 'tempo_entrega_pedido')
+
+pedido_certo = ctrl.Antecedent(np.arange(0, 11, 1), 'pedido_certo')
+
+temperatura_da_comida = ctrl.Antecedent(np.arange(0, 11, 1), 'temperatura_da_comida')
+
+sabor = ctrl.Antecedent(np.arange(0, 11, 1), 'sabor')
+
+# antecedentes relacionados com a estrutura do restaurante
+estacionamento = ctrl.Antecedent(np.arange(0, 11, 1), 'estacionamento')
+
+quantidade_de_mesas = ctrl.Antecedent(np.arange(0, 11, 1), 'quantidade_de_mesas')
+
+temperatura_do_ambiente = ctrl.Antecedent(np.arange(0, 11, 1), 'temperatura_do_ambiente')
+
 qualidade_atendimento.automf(3)
 tempo_entrega_pedido.automf(3)
 pedido_certo.automf(3)
@@ -30,7 +36,8 @@ sabor.automf(3)
 estacionamento.automf(3)
 quantidade_de_mesas.automf(3)
 temperatura_do_ambiente.automf(3)
-'''
+
+
 # consequente
 # satisfação 0-10
 
@@ -59,20 +66,23 @@ temperatura_do_ambiente.view()
 
 # Definindo as regras de agregação
 # satisfação baixa
-regra1 = ctrl.Rule(qualidade_atendimento['ruim'] | tempo_entrega_pedido['longo'] & pedido_certo['totalmente diferente'] )
-
+regra1 = ctrl.Rule(qualidade_atendimento['poor'] & tempo_entrega_pedido['poor'] & pedido_certo['poor'] & temperatura_da_comida['poor'] & sabor['poor'] & estacionamento['poor'] & quantidade_de_mesas['poor'] & temperatura_do_ambiente['poor'], satisfacao['baixa'])
+regra2 = ctrl.Rule(qualidade_atendimento['average'] & tempo_entrega_pedido['average'] & pedido_certo['good'] & temperatura_da_comida['good'] & sabor['average'] & estacionamento['poor'] & quantidade_de_mesas['average'] & temperatura_do_ambiente['poor'],satisfacao['baixa'])
+regra3 = ctrl.Rule(qualidade_atendimento['poor'] & tempo_entrega_pedido['good'] & pedido_certo['good'] & temperatura_da_comida['good'] & sabor['average'] & estacionamento['average'] & quantidade_de_mesas['good'] & temperatura_do_ambiente['average'],satisfacao['baixa'])
 # indiferente
 
-regra2 = ctrl.Rule(qualidade_atendimento['bom'] & tempo_entrega_pedido['um pouco demorado'] | pedido_certo['um pouco diferente'] )
-
+regra4 = ctrl.Rule(qualidade_atendimento['average'] & tempo_entrega_pedido['average'] & pedido_certo['average'] & temperatura_da_comida['average'] & sabor['average'] & estacionamento['average'] & quantidade_de_mesas['average'] & temperatura_do_ambiente['average'],satisfacao['indiferente'])
+regra5 = ctrl.Rule(qualidade_atendimento['average'] & tempo_entrega_pedido['good'] & pedido_certo['average'] & temperatura_da_comida['average'] & sabor['average'] & estacionamento['poor'] & quantidade_de_mesas['poor'] & temperatura_do_ambiente['average'],satisfacao['indiferente'])
+regra6 = ctrl.Rule(qualidade_atendimento['average'] & tempo_entrega_pedido['average'] & pedido_certo['good'] & temperatura_da_comida['good'] & sabor['average'] & estacionamento['poor'] & quantidade_de_mesas['poor'] & temperatura_do_ambiente['average'],satisfacao['indiferente'])
 # satisfação alta
 
-regra3 = ctrl.Rule(qualidade_atendimento['otimo'] | tempo_entrega_pedido['rapido'] | pedido_certo['igual'])
+regra7 = ctrl.Rule(qualidade_atendimento['good'] & tempo_entrega_pedido['good'] & pedido_certo['good'] & temperatura_da_comida['good'] & sabor['good'] & estacionamento['good'] & quantidade_de_mesas['good'] & temperatura_do_ambiente['good'],satisfacao['alta'])
+regra8 = ctrl.Rule(qualidade_atendimento['good'] & tempo_entrega_pedido['average'] & pedido_certo['good'] & temperatura_da_comida['good'] & sabor['good'] & estacionamento['good'] & quantidade_de_mesas['good'] & temperatura_do_ambiente['good'],satisfacao['alta'])
+regra9 = ctrl.Rule(qualidade_atendimento['good'] & tempo_entrega_pedido['good'] & pedido_certo['good'] & temperatura_da_comida['good'] & sabor['good'] & estacionamento['good'] & quantidade_de_mesas['average'] & temperatura_do_ambiente['good'],satisfacao['alta'])
 
 
 
-
-satisfacao_tipping_ctrl = ctrl.ControlSystem([regra1, regra2, regra3])
+satisfacao_tipping_ctrl = ctrl.ControlSystem([regra1, regra2, regra3, regra4, regra5, regra6, regra7, regra8, regra9])
 satisfacao_tipping = ctrl.ControlSystemSimulation(satisfacao_tipping_ctrl)
 
 def grau_de_satisfacao(satisfacao):
@@ -90,6 +100,11 @@ def grau_de_satisfacao(satisfacao):
 satisfacao_tipping.input['qualidade_atendimento'] = 5.0
 satisfacao_tipping.input['tempo_entrega_pedido'] = 2.0
 satisfacao_tipping.input['pedido_certo'] = 10.0
+satisfacao_tipping.input['temperatura_da_comida'] = 6.0
+satisfacao_tipping.input['sabor'] = 8.0
+satisfacao_tipping.input['estacionamento'] = 9.0
+satisfacao_tipping.input['quantidade_de_mesas'] = 4.0
+satisfacao_tipping.input['temperatura_do_ambiente'] = 9.0
 satisfacao_tipping.compute()
 
 print(satisfacao_tipping.output['satisfacao'])
@@ -101,6 +116,11 @@ satisfacao.view(sim=satisfacao_tipping)
 satisfacao_tipping.input['qualidade_atendimento'] = 8.0
 satisfacao_tipping.input['tempo_entrega_pedido'] = 7.0
 satisfacao_tipping.input['pedido_certo'] = 10.0
+satisfacao_tipping.input['temperatura_da_comida'] = 9.0
+satisfacao_tipping.input['sabor'] = 6.0
+satisfacao_tipping.input['estacionamento'] = 7.0
+satisfacao_tipping.input['quantidade_de_mesas'] = 9.0
+satisfacao_tipping.input['temperatura_do_ambiente'] = 8.0
 satisfacao_tipping.compute()
 
 print(satisfacao_tipping.output['satisfacao'])
@@ -112,6 +132,11 @@ satisfacao.view(sim=satisfacao_tipping)
 satisfacao_tipping.input['qualidade_atendimento'] = 10.0
 satisfacao_tipping.input['tempo_entrega_pedido'] = 3.0
 satisfacao_tipping.input['pedido_certo'] = 7.0
+satisfacao_tipping.input['temperatura_da_comida'] = 8.0
+satisfacao_tipping.input['sabor'] = 7.0
+satisfacao_tipping.input['estacionamento'] = 1.0
+satisfacao_tipping.input['quantidade_de_mesas'] = 5.0
+satisfacao_tipping.input['temperatura_do_ambiente'] = 4.0
 satisfacao_tipping.compute()
 
 print(satisfacao_tipping.output['satisfacao'])
